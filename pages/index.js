f145557a582157a98297fc29dd74e1abd6e5102a
gillcheese <70168685+gillcheese@users.noreply.github.com>
2024-07-18 18:22:25 -0400

    styling

--- pages/index.js
+++ pages/index.js
@@ -97,9 +97,9 @@ export default function Home() {
         <div className="absolute inset-0 bg-gradient-to-r from-cyan-400 to-light-blue-500 shadow-lg transform -skew-y-6 sm:skew-y-0 sm:-rotate-6 sm:rounded-3xl"></div>
         <div className="relative px-4 py-10 bg-white shadow-lg sm:rounded-3xl sm:p-20">
           <div className="max-w-md mx-auto">
-            <div className="text-center">
-              <h1 className="text-2xl font-semibold mb-6">BOOP!</h1>
-            </div>
+           <div className="text-center">
+  <h1 className="text-9xl font-reenie mb-0">Boop.</h1>
+</div>
             <div className="divide-y divide-gray-200">
               <div className="py-8 text-base leading-6 space-y-4 text-gray-700 sm:text-lg sm:leading-7">
                 <div className="flex justify-center mb-4">
--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,12 @@ module.exports = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      fontFamily: {
+        'reenie': ['"Reenie Beanie"', 'cursive'],
+      },
+      fontSize: {
+        '9xl': '8rem',
+      },
     },
   },
   plugins: [],
