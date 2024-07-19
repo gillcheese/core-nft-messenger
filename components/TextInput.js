7cc23ffa851a2a621b3ea51cccc08fa9268c0d89
gillcheese <70168685+gillcheese@users.noreply.github.com>
2024-07-19 18:34:20 -0400

    text field ux

--- components/TextInput.js
+++ components/TextInput.js
@@ -14,6 +14,7 @@ const TextInput = ({ onTextChange }) => {
       value={text}
       onChange={handleChange}
       placeholder="Enter your text here"
+      className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500 transition duration-200"
     />
   );
 };
